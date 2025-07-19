0dd7335d447258e023c78c6c02cee24afed91f97
Kartik Bhatnagar <bhatnagark434@gmail.com>
2025-07-19 12:32:57 +0530

    Add files via upload

--- number guessing game .py
+++ number guessing game .py
@@ -3,7 +3,7 @@ r= random.randint(1,10)
 print("THIS IS A NUMBER GUESSSING GAME")
 
 while True:
-    userchoice=int(input("ENTER YOUR GUESS between 1 to 10:"))
+    userchoice=int(input("ENTER YOUR GUESS BETWEEN 1 TO 10:"))
 
     if (userchoice==r):
      print("CORRECT GUESS")
