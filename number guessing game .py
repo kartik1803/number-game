3fa76483afeb3e90c3c70c016312687a609b6a42
kartik1803 <bhatnagark434@gmail.com>
2025-07-19 11:44:44 +0530

    Add files via upload

---
+++ number guessing game .py
@@ -0,0 +1,19 @@
+import random
+r= random.randint(1,10)  # system choese a random number between 1 to 10
+print("THIS IS A NUMBER GUESSSING GAME")
+
+while True:
+    userchoice=int(input("ENTER YOUR GUESS:"))
+
+    if (userchoice==r):
+     print("CORRECT GUESS")
+     break 
+   
+    elif(userchoice <r):
+       print("GUESS TOO LOW ")
+
+    else:
+       print("GUESS TOO HIGH")
+      
+            
+print ("----------GAME OVER TRY AGAIN---------------")
